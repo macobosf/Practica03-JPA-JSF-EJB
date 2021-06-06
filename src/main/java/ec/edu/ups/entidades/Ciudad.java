00b8a3f4c7f6fedafd3af952450dc123340e3f65
Marco Antonio Cobos Farfan <49213774+macobosf@users.noreply.github.com>
2021-06-06 18:51:41 -0500

    Bodega1

---
+++ src/main/java/ec/edu/ups/entidades/Ciudad.java
@@ -0,0 +1,21 @@
+package ec.edu.ups.entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Ciudad
+ *
+ */
+@Entity
+
+public class Ciudad implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Ciudad() {
+		super();
+	}
+   
+}
---
+++ src/main/java/ec/edu/ups/entidades/Producto.java
@@ -0,0 +1,21 @@
+package ec.edu.ups.entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Producto
+ *
+ */
+@Entity
+
+public class Producto implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Producto() {
+		super();
+	}
+   
+}
